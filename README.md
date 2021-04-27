5b0bf5487336845ff8b0109af4f392eb0a8ce82a
abudimirov <alexys077@gmail.com>
2021-04-27 22:26:17 +0300

    added basic readme

---
+++ README.md
@@ -0,0 +1,19 @@
+Deutsche Telekom - Reha project
+=====================
+
+Description
+-----------------------------------
+
+This app helps medical personnel of a rehabilitation center with document management. 
+
+Admin part of the app is for doctors and nurses.
+
+###User roles
+
+**Doctor**
+
+Doctor can add new patients of a clinic and edit existing ones. Doctors have rights to set medical treatment and schedule procedures for patients. 
+
+**Nurse**
+
+Nurse can view scheduled procedures and change its status. 
--- src/main/java/cabinet/dao/PatientDAOImpl.java
+++ src/main/java/cabinet/dao/PatientDAOImpl.java
@@ -7,16 +7,13 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Set;
 
 @Repository
 public class PatientDAOImpl implements PatientDAO {
     @PersistenceContext
-    private EntityManager entityManager;
     private SessionFactory sessionFactory;
 
     @Autowired
@@ -26,21 +23,18 @@ public class PatientDAOImpl implements PatientDAO {
     }
 
     @SuppressWarnings("unchecked")
-    //@Transactional
     public List<Patient> allPatients(int page) {
         Session session = sessionFactory.getCurrentSession();
         return session.createQuery("from Patient p").setFirstResult(10 * (page - 1)).setMaxResults(10).list();
     }
 
     @Override
-    //@Transactional
     public void add(Patient patient) {
         Session session = sessionFactory.getCurrentSession();
         session.persist(patient);
     }
 
     @Override
-    //@Transactional
     public void discharge(Patient patient) {
         Session session = sessionFactory.getCurrentSession();
         patient.setCured(true);
@@ -54,21 +48,18 @@ public class PatientDAOImpl implements PatientDAO {
     }
 
     @Override
-    //@Transactional
     public void edit(Patient patient) {
         Session session = sessionFactory.getCurrentSession();
         session.update(patient);
     }
 
     @Override
-    //@Transactional
     public Patient getById(int id) {
         Session session = sessionFactory.getCurrentSession();
         return session.get(Patient.class, id);
     }
 
     @Override
-    //@Transactional
     public int patientsCount() {
         Session session = sessionFactory.getCurrentSession();
         return session.createQuery("select count(*) from Patient ", Number.class).getSingleResult().intValue();
